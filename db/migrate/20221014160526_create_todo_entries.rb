13d24307082d101517243f6f2c8834f1c044b794
msp230511 <mspieler@colgate.edu>
2022-10-14 12:08:10 -0400

    Created Todo Model

---
+++ db/migrate/20221014160526_create_todo_entries.rb
@@ -0,0 +1,16 @@
+class CreateTodoEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todo_entries do |t|
+      # t.references :user
+      t.string :name
+      t.text :description
+      t.string :priority
+      t.string :category
+      t.date :due_at
+      t.boolean :completed, null: false
+ 
+      t.timestamps
+    end
+  end
+end
+
