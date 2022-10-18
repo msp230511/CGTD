a69d88a287aaf3d1c6182560aa683902868951bb
zliu <zliu@colgate.edu>
2022-10-17 21:40:33 -0400

    Working on basic function/view in index

--- app/controllers/todo_entries_controller.rb
+++ app/controllers/todo_entries_controller.rb
@@ -1,2 +1,7 @@
 class TodoEntriesController < ApplicationController
+    def index
+        @todo_entries = TodoEntry.all
+        order = params[:order] || 'name'
+        @todo_entries = TodoEntry.sorted_by(order)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :todo_entries
+  root 'todo_entries#index'
 end
