fa3fe2f136b56db243ba99377663644b057848f9
Maximilian Spieler <max@thespielers.com>
2022-10-17 23:16:18 -0400

    Added Data Seeds, Cleaned up Views Folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+TodoEntry.delete_all
+
+TodoEntry.create!(name: "Do Laundry", description: "Do all laundry by tonight", priority: "medium",category: "personal",due_at: "#{Time.now}",completed:false)
+TodoEntry.create!(name: "Walk the Dog", description: "Mom said if I don't do it she will be really mad", priority: "high",category: "personal",due_at: "#{Time.now}",completed:false)
+TodoEntry.create!(name: "Finish Math Homework", description: "Chapter 3, Sec. 5-10", priority: "high",category: "work",due_at: "#{Time.now}",completed:false)
+TodoEntry.create!(name: "Learn how to do a Backflip", description: "Seriously: https://www.youtube.com/watch?v=ltho8_PzC2U", priority: "low",category: "personal",due_at: "#{Time.now}",completed:false)
+TodoEntry.create!(name: "Finish the Dishes", description: "The bowls are lookin kinda sus", priority: "low",category: "personal",due_at: "#{Time.now}",completed:false)
+TodoEntry.create!(name: "Meditate for 20 minutes", description: "Gotta do it", priority: "low",category: "personal",due_at: "#{Time.now}",completed:false)
