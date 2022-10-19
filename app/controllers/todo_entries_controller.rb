739275f31c2c12a260c643a3579497181116040e
Kaleabe Abera <kabera@colgate.edu>
2022-10-19 12:38:40 -0400

    fixedMerge

--- app/controllers/todo_entries_controller.rb
+++ app/controllers/todo_entries_controller.rb
@@ -22,20 +22,17 @@ class TodoEntriesController < ApplicationController
         flash[:alert] = 'Failed to save new task'
         redirect_to new_todo_entry_path
       end
-<<<<<<< HEAD
 
     def show
         @todo_entries = TodoEntry.all
         id = params[:id]
         @todo_entries = TodoEntry.find(id)
     end
-=======
-    end
+
 
     # private
 
     # def check_params
     #   params.require(:todo_entry).permit(:name, :description, :priority, :category, :due_at, :completed)
     # end
->>>>>>> afba47b0480d1f5b0a249c8e88557f6dfd206eba
 end
