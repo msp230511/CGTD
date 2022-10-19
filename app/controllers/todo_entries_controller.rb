ad429eec1d85b76ec56c64a36ed19fc029fdc194
Maximilian Spieler <max@thespielers.com>
2022-10-19 17:56:57 -0400

    Added Complete and Task History

--- app/controllers/todo_entries_controller.rb
+++ app/controllers/todo_entries_controller.rb
@@ -1,37 +1,70 @@
 class TodoEntriesController < ApplicationController
-    def index
-        @todo_entries = TodoEntry.all
-        order = params[:order] || 'name'
-        @todo_entries = TodoEntry.sorted_by(order)
-    end
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
-    def new
-      @todo_entry = TodoEntry.new
-    end
+  def index
+      order = params[:order] || 'name'
+      @todo_entries = TodoEntry.all.sorted_by(order)
+  end
 
-    def create
-      td = TodoEntry.new(check_params)
-      # td = TodoEntry.new
-      td.completed = false
-      puts params
-      puts "AFTER PARAMS"
-      if td.save
-        flash[:notice] = "New task #{td.name} created"
-        redirect_to todo_entries_path
-      else
-        flash[:alert] = 'Failed to save new task'
-        redirect_to new_todo_entry_path
-      end
-    end
+  def new
+    @todo_entry = TodoEntry.new
+  end
 
-    def show
-        id = params[:id]
-        @todo_entry = TodoEntry.all.find(id)
-    end
 
-    # private
+  def show
+    id = params[:id]
+    @todo_entry = TodoEntry.find(id)
+  end
 
-    def check_params
-      params.require(:todo_entry).permit(:name, :description, :priority, :category, :due_at, :completed)
+  def edit
+    @todo_entry = TodoEntry.find(params[:id])
+  end
+
+  def complete
+    @todo_entry = TodoEntry.find(params[:id])
+    @todo_entry.completed = true
+    @todo_entry.save
+    flash[:notice] = "Successfully Completed Task: #{@todo_entry.name}."
+    redirect_to todo_entries_path()
+  end
+
+  def undo_complete
+    @todo_entry = TodoEntry.find(params[:id])
+    @todo_entry.completed = false
+    @todo_entry.save
+    flash[:notice] = "Successfully Reactivated Task: #{@todo_entry.name}."
+    redirect_to todo_entries_path()
+  end
+
+  def update 
+    @todo_entry = TodoEntry.find(params[:id])
+    @todo_entry.update(check_params)
+    flash[:notice] = "Task: #{@todo_entry.name} : was successfully updated."
+    redirect_to todo_entry_path(@todo_entry)
+  end
+
+  def create
+    td = TodoEntry.new(check_params)
+    # td = TodoEntry.new
+    td.completed = false
+    if td.save
+      flash[:notice] = "New task #{td.name} created"
+      redirect_to todo_entries_path
+    else
+      flash[:alert] = 'Failed to save new task'
+      redirect_to new_todo_entry_path
     end
+  end
+
+  private
+
+  def record_not_found
+    flash[:alert] = 'No such toy'
+    redirect_to products_path
+  end
+
+  def check_params
+    params.require(:todo_entry).permit(:name, :description, :priority, :category, :due_at, :completed)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   resources :todo_entries
   root 'todo_entries#index'
   post "todo_entries/new", to: "todo_entries#new"
+
+  resources :todo_entries do
+    member do
+      get 'complete'
+      get 'undo_complete'
+    end
+  end
 end
