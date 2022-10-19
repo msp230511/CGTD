ee5e0f9e83d08a71368e37b9ad3546c8607b87a6
Kaleabe Abera <kabera@colgate.edu>
2022-10-19 12:33:34 -0400

    KaleabeShow

--- app/controllers/todo_entries_controller.rb
+++ app/controllers/todo_entries_controller.rb
@@ -4,4 +4,10 @@ class TodoEntriesController < ApplicationController
         order = params[:order] || 'name'
         @todo_entries = TodoEntry.sorted_by(order)
       end
+
+    def show
+        @todo_entries = TodoEntry.all
+        id = params[:id]
+        @todo_entries = TodoEntry.find(id)
+    end
 end
