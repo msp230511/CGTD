fb96cf806575184ecb311fd8903338b1ec3fe86e
Maximilian Spieler <max@thespielers.com>
2022-10-22 14:10:38 -0400

    Cuked "Complete" and "Reactivate" Actions

---
+++ features/step_definitions/feature_steps.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# PRECONDITIONS & PRE-CONFIGURATION --------------------------------
+Given('these tasks:') do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  table.hashes.each do |h|
+    TodoEntry.create!(h)
+  end
+end
+
+Given('I am on the index page') do
+  visit(todo_entries_path)
+end
+
+# LOCATION ---------------------------------------------------------
+Then('I should be on the index page') do
+  expect(page.current_path).to eq(todo_entries_path).or eq('/')
+end
+
+# ACTIONS ----------------------------------------------------------
+When('I click the {string} button for the task {string}') do |button_class, task|
+  found = false
+  found = true unless (raifind('tr', text: task).find(".#{button_class}").click).nil?
+  expect(found).to be(true)
+end
+
+# STATES -----------------------------------------------------------
+Then('{string} should be an active task') do |string|
+  expect(all('td.task_name').any? { |td| td.text == string }).to eq(true)
+end
+
+Then('{string} should be a completed task') do |string|
+  expect(all('td.completed_task_name').any? { |td| td.text == string }).to eq(true)
+end
