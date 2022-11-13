0c04043863e13f913ebb0c6bd0d1a9c63176ba94
Maximilian Spieler <max@thespielers.com>
2022-11-13 12:03:52 -0500

    Seed Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,24 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-# User.delete_all
+TodoEntry.delete_all
+TodoList.delete_all
+User.delete_all
 
-# User.create!(email: "admin@colgate.edu", password: "colgate13")
-# TodoList.create!(list_name: "List1", user_id: 1)
+user = User.create!(email: "admin@colgate.edu", password: "colgate13")
+todo_list = TodoList.create!(list_name: "List1", user_id: user.id)
 
-# TodoEntry.create!(name: 'Do Laundry', description: 'Do all laundry by tonight', priority: 2, category: 'Personal',
-#                   due_at: Time.now.advance(days: 1).to_s, completed: false)
-# TodoEntry.create!(name: 'Walk the Dog', description: "Mom said if I don't do it she will be really mad", priority: 3,
-#                   category: 'Personal', due_at: Time.now.advance(hours: 5).to_s, completed: false)
-# TodoEntry.create!(name: 'Finish Math Homework', description: 'Chapter 3, Sec. 5-10', priority: 3, category: 'Work',
-#                   due_at: Time.now.advance(days: 5).to_s, completed: false)
-# TodoEntry.create!(name: 'Learn how to do a Backflip',
-#                   description: 'Seriously: https://www.youtube.com/watch?v=ltho8_PzC2U', priority: 1, category: 'Personal', due_at: Time.now.advance(days: 10).to_s, completed: false)
-# TodoEntry.create!(name: 'Finish the Dishes', description: 'The bowls are lookin kinda sus', priority: 1,
-#                   category: 'Personal', due_at: Time.now.advance(hours: 3).to_s, completed: false)
-# TodoEntry.create!(name: 'Meditate for 20 minutes', description: 'Gotta do it', priority: 1, category: 'Personal',
-#                   due_at: Time.now.advance(hours: 1).to_s, completed: false)
-# TodoEntry.create!(name: 'Finish CSS Styling for This App!', description: 'Gotta do it.', priority: 3, category: 'Work',
-#                   due_at: Time.now.advance(days: 5).to_s, completed: false)
+TodoEntry.create!(name: 'Do Laundry', description: 'Do all laundry by tonight', priority: 2, category: 'Personal',
+                  due_at: Time.now.advance(days: 1).to_s, completed: false, todo_list_id: todo_list.id)
+TodoEntry.create!(name: 'Walk the Dog', description: "Mom said if I don't do it she will be really mad", priority: 3,
+                  category: 'Personal', due_at: Time.now.advance(hours: 5).to_s, completed: false, todo_list_id: todo_list.id)
+TodoEntry.create!(name: 'Finish Math Homework', description: 'Chapter 3, Sec. 5-10', priority: 3, category: 'Work',
+                  due_at: Time.now.advance(days: 5).to_s, completed: false, todo_list_id: todo_list.id)
+TodoEntry.create!(name: 'Learn how to do a Backflip',
+                  description: 'Seriously: https://www.youtube.com/watch?v=ltho8_PzC2U', priority: 1, category: 'Personal', due_at: Time.now.advance(days: 10).to_s, completed: false, todo_list_id: todo_list.id)
+TodoEntry.create!(name: 'Finish the Dishes', description: 'The bowls are lookin kinda sus', priority: 1,
+                  category: 'Personal', due_at: Time.now.advance(hours: 3).to_s, completed: false, todo_list_id: todo_list.id)
+TodoEntry.create!(name: 'Meditate for 20 minutes', description: 'Gotta do it', priority: 1, category: 'Personal',
+                  due_at: Time.now.advance(hours: 1).to_s, completed: false, todo_list_id: todo_list.id)
+TodoEntry.create!(name: 'Finish CSS Styling for This App!', description: 'Gotta do it.', priority: 3, category: 'Work',
+                  due_at: Time.now.advance(days: 5).to_s, completed: false, todo_list_id: todo_list.id)
